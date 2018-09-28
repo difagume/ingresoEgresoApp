11e7d499bdb3dc5e1df7e9e77de806e353a062e8
Diego Guzmán <dfguzman@ceaaces.gob.ec>
2018-09-28 14:52:37 -0500

    Obtener el UID de nuestros elementos de Firebase (9 - 101)

--- src/app/ingreso-egreso/ingreso-egreso.service.ts
+++ src/app/ingreso-egreso/ingreso-egreso.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Store } from '@ngrx/store';
-import { filter } from 'rxjs/operators';
+import { filter, map } from 'rxjs/operators';
 import { AppState } from '../app.reducer';
+import { SetItemsAction } from './ingreso-egreso.actions';
 import { IngresoEgreso } from './ingreso-egreso.model';
 import { Subscription } from 'rxjs';
 
@@ -11,13 +12,16 @@ import { Subscription } from 'rxjs';
 })
 export class IngresoEgresoService {
 
+  ingresoEgresoListenerSubscription: Subscription = new Subscription();
+  ingresoEgresoItemsSubscription: Subscription = new Subscription();
+
   constructor(private afDB: AngularFirestore,
     private store: Store<AppState>) { }
 
 
   initIngresoEgresoListener() {
 
-    this.store.select('auth')
+    this.ingresoEgresoListenerSubscription = this.store.select('auth')
       .pipe(
         filter(auth => auth.user != null) // Permite establecer una condición, si se cumple deja pasar, si no se cumple no pasa
       )
@@ -26,11 +30,30 @@ export class IngresoEgresoService {
 
 
   private ingresoEgresoItems(uid: string) {
-    this.afDB.collection(`${uid}/ingresos-egresos/items`)
-      .valueChanges()
-      .subscribe(items => {
-        console.log(items);
+    this.ingresoEgresoItemsSubscription = this.afDB.collection(`${uid}/ingresos-egresos/items`)
+      .snapshotChanges()
+      .pipe(
+        map(docData => {
+
+          return docData.map(doc => {
+            return {
+              uid: doc.payload.doc.id,
+              ...doc.payload.doc.data()
+            };
+          });
+
+        })
+      )
+      .subscribe((coleccion: any[]) => {
+        // console.log(coleccion);
+        this.store.dispatch(new SetItemsAction(coleccion));
       });
+  }
+
+
+  cancelarSubscriptions() {
+    this.ingresoEgresoListenerSubscription.unsubscribe();
+    this.ingresoEgresoItemsSubscription.unsubscribe();
   }
 
 
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
+import { IngresoEgresoService } from '../../ingreso-egreso/ingreso-egreso.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,12 +9,14 @@ import { AuthService } from '../../auth/auth.service';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService,
+    private ingresoEgresoService: IngresoEgresoService) { }
 
   ngOnInit() {
   }
 
   logout() {
+    this.ingresoEgresoService.cancelarSubscriptions();
     this.authService.logout();
   }
 
