9848fcd98614bce18bc18c95b694864d70eb3396
Diego Guzmán <dfguzman@ceaaces.gob.ec>
2018-09-28 16:47:54 -0500

    Colocar el nombre del usuario activo

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
+import { AppState } from '../../app.reducer';
 import { AuthService } from '../../auth/auth.service';
 import { IngresoEgresoService } from '../../ingreso-egreso/ingreso-egreso.service';
 
@@ -7,12 +11,25 @@ import { IngresoEgresoService } from '../../ingreso-egreso/ingreso-egreso.servic
   templateUrl: './sidebar.component.html',
   styles: []
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, OnDestroy {
+
+  nombre: string;
+  subscription: Subscription = new Subscription();
 
   constructor(private authService: AuthService,
-    private ingresoEgresoService: IngresoEgresoService) { }
+    private ingresoEgresoService: IngresoEgresoService,
+    private store: Store<AppState>) { }
 
   ngOnInit() {
+    this.subscription = this.store.select('auth')
+      .pipe(
+        filter(auth => auth.user != null)
+      )
+      .subscribe(auth => this.nombre = auth.user.nombre);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   logout() {
