9f7caf07d2899de795207ede299e4fee5ad2838d
Diego Fabricio <difagume@gmail.com>
2018-09-28 23:04:28 -0500

    CanLoad del authGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuardService } from './auth/auth-guard.service';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 
 const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-    { path: '', loadChildren: './ingreso-egreso/ingreso-egreso.module#IngresoEgresoModule' },
+    {
+        path: '',
+        loadChildren: './ingreso-egreso/ingreso-egreso.module#IngresoEgresoModule',
+        canLoad: [AuthGuardService]
+    },
     { path: '**', redirectTo: '' }
 ];
 
